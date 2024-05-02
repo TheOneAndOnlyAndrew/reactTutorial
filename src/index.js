ab77b78d302d923cdaf857657cf110a900fdfe1c
Andrew <andrew@coding>
2024-05-01 21:31:56 -0400

    React props part 2

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-    const inputType = "number";
-    const minValue = 5;
-
-    return <input type={inputType} min={minValue} />;
+    return <input type="number" min={5}/>
 }
 
 // 5) Show the component on the screen
